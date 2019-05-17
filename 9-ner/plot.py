97dd1518f3954a8eeee06e0242a401f0b772caa5
Katarzyna Banaszak <kokanuga@gmail.com>
2019-05-17 23:49:03 +0200

    minor changes in plots

--- 9-ner/plot.py
+++ 9-ner/plot.py
@@ -4,20 +4,20 @@ import pandas as pd
 coarse = False
 
 if coarse:
-	n = 'coarse_histo'
+	n = 'coarse'
 else:
-	n = 'fine_histo'
+	n = 'fine'
 
-data = pd.read_csv(n + '.csv', sep=',',header=None, index_col =0)
+data = pd.read_csv(n + '.csv', sep=',', header=None, index_col=0)
 
-data.plot(kind='bar')
+data.plot(kind='bar', legend=False)
 plt.ylabel('Frequency')
 plt.xlabel('Class')
 
 if coarse:
-	plt.title('Coarse-grained classification histogram')
+	plt.title('Coarse-grained classification')
 else:
-	plt.title('Fine-grained classification histogram')
+	plt.title('Fine-grained classification')
 plt.tight_layout()
 
 plt.savefig(n + '.svg')
